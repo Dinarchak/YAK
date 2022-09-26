6ec554eda45841fe5b51ab8ff3e154cf1ae355ef
Dinarhak <jjustdinar@gmail.com>
2022-09-26 17:32:01 +0300

    Progect upload in git

---
+++ static/js/javascript.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    adaptiveHeight:true,
+    centerMode: true,
+    arrows: true
+  });
+});
+
+console.log('ok')
+	

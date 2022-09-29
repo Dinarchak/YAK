5f5f52ed4d7c7bc46ec2216d35fe92d6ab8de611
Dinarhak <jjustdinar@gmail.com>
2022-09-30 00:19:34 +0300

    Удалены настройки слайдера

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,14 +1,2 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    adaptiveHeight:true,
-    centerMode: true,
-    arrows: true
-  });
-});
-
 console.log('ok')
 	

c5882e91eb1229fad2e073eb360bbfe865622692
Dinarhak <jjustdinar@gmail.com>
2022-09-30 17:43:44 +0300

    Добавлен прогресбар при прокрутке страницы

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,2 +1,18 @@
+function progressBar() {
+    // Узнаем на сколько страница прокручена
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    // Узнаем высоту всей страницы
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    // Получаем в % на сколько прокручена страница
+    let scrolled = scroll / height * 100;
+
+    // Подставляем % прокрутки в ширину нашей линии
+    document.querySelector('.progress-bar').style.width = scrolled + '%';
+}
+
+// Запускаем функцию, когда пользователя скролит
+window.addEventListener('scroll', progressBar);
+
+
 console.log('ok')
 	
